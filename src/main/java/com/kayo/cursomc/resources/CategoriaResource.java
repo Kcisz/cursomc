c2b09f00a40be254bf37790b7e00c15dc95efd51
Kayo Cezar <kayocisz@gmail.com>
2019-02-22 17:43:17 -0400

    Criando repository e service para Categoria

--- src/main/java/com/kayo/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/kayo/cursomc/resources/CategoriaResource.java
@@ -1,28 +1,29 @@
 package com.kayo.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kayo.cursomc.domain.Categoria;
+import com.kayo.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
+	@Autowired
+	private CategoriaService service;
 	
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Categoria> listar() {
-		Categoria cat1 = new Categoria(1,"Informatica");
-		Categoria cat2 = new Categoria(2,"Escritótio");
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> finf(@PathVariable Integer id) {
 		
-		List<Categoria> lista = new ArrayList<>(); 
+		Categoria obj = service.find(id);
 		
-		lista.add(cat1);
-		lista.add(cat2);
-		return lista;
+		
+		return ResponseEntity.ok().body(obj);
+		
 	}
 }
