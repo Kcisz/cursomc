0e12066849529b7a521f326abf19791749d614fa
Kayo Cezar <kayocisz@gmail.com>
2019-02-22 16:00:03 -0400

    Testando a primeira classe de dominio - Categoria

--- src/main/java/com/kayo/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/kayo/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.kayo.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.kayo.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -10,7 +15,14 @@ public class CategoriaResource {
 	
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1,"Informatica");
+		Categoria cat2 = new Categoria(2,"Escritótio");
+		
+		List<Categoria> lista = new ArrayList<>(); 
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
 }
