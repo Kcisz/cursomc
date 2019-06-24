f4a294553ed7cbcca3091bcea247b10aa0f2dd0c
Kayo Cezar <kayocisz@gmail.com>
2019-06-24 17:38:11 -0400

    Cliente, TipoCliente, Telefones e enderecos

--- src/main/java/com/kayo/cursomc/CursomcApplication.java
+++ src/main/java/com/kayo/cursomc/CursomcApplication.java
@@ -1,8 +1,6 @@
 package com.kayo.cursomc;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -10,10 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.kayo.cursomc.domain.Categoria;
 import com.kayo.cursomc.domain.Cidade;
+import com.kayo.cursomc.domain.Cliente;
+import com.kayo.cursomc.domain.Endereco;
 import com.kayo.cursomc.domain.Estado;
 import com.kayo.cursomc.domain.Produto;
+import com.kayo.cursomc.domain.enums.TipoCliente;
 import com.kayo.cursomc.repositories.CategoriaRepository;
 import com.kayo.cursomc.repositories.CidadeRepository;
+import com.kayo.cursomc.repositories.ClienteRepository;
+import com.kayo.cursomc.repositories.EnderecoRepository;
 import com.kayo.cursomc.repositories.EstadoRepository;
 import com.kayo.cursomc.repositories.ProdutoRepository;
 
@@ -25,12 +28,18 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private ProdutoRepository produtoRepository;
-	
+
 	@Autowired
 	private EstadoRepository estadoRepository;
-	
+
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -54,7 +63,7 @@ public class CursomcApplication implements CommandLineRunner {
 
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
-		
+
 		Estado est1 = new Estado(null, "Minas Gerais");
 		Estado est2 = new Estado(null, "São Paulo");
 
@@ -65,8 +74,22 @@ public class CursomcApplication implements CommandLineRunner {
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2, c3));
 
-		estadoRepository.saveAll(Arrays.asList(est1,est2));
-		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+
+		Cliente cli1 = new Cliente(null, "Maria Silva", "Maria.nunes2gmail.com", "36378912377",
+				TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93948997"));
+
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardins", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1,e2));
+
 	}
 
 }
